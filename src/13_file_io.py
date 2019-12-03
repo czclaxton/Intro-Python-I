3db680e963a943a8e984972cb3620052e9c9b367
Connor Claxton <czclaxton@gmail.com>
2019-12-02 18:25:49 -0600

    finished up to 14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,17 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open(r'./src/foo.txt', "r")
+
+print(f.read())
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+g = open(r'./src/bar.txt', "w+")
+g.write('asdfasdfasd\nfasdfasdfasdfas\ndfasdfasdf')
+g.close()
